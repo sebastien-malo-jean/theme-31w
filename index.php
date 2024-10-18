ec983e28fa931162fbb8c4ad05dc1d38ac9eb5d8
Sebastien Malo Jean <E2296673@cmaisonneuve.qc.ca>
2024-10-17 21:26:39 -0400

    l'affichage des blocs est Terminer;

--- index.php
+++ index.php
@@ -32,19 +32,28 @@
     <main class="principal">
         <section class="global">
             <h2>Accueil</h2>
-            <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post() ?>
-            <?php
+            <div class="principal__conteneur">
+                <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post() ?>
+                <?php
                 $chaine = get_the_title();
                 $sigle = substr($chaine,0,7);
-                $titre = substr($chaine, 8,80);
+                $titre = substr($chaine, 8,stripos($chaine,"(")-8);
             ?>
-            <article class="principal__article">
-                <h5><?= $sigle ?></h5>
-                <small><?= $titre ?></small>
-                <p><?php  echo wp_trim_words(get_the_excerpt(), 30, null); ?></p>
-            </article>
-            <?php endwhile; ?>
+                <article class="principal__article">
+                    <h5><?= $sigle ?></h5>
+                    <h6><?= $titre ?></h6>
+                    <p><?php  echo wp_trim_words(get_the_excerpt(), 30, null); ?></p>
+                    <?php
+                    $pos_ouvrante = stripos($chaine, "(");
+                    if ($pos_ouvrante !== false) {
+                        $heureDemandé = substr($chaine, $pos_ouvrante + 1, -1);
+                    }
+                ?>
+                    <small>(<?= $heureDemandé ?>)</small>
+                </article>
+                <?php endwhile; ?>
+            </div>
             <?php endif ?>
         </section>
     </main>

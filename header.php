ade17f0a848ead41e62fb45b60395db47cf499f2
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2024-10-27 16:32:47 -0400

    test de commit

--- header.php
+++ header.php
@@ -15,16 +15,23 @@
 <body>
     <header class="entete">
         <section class="global">
-            <h1><?php bloginfo('name')?></h1>
+            <h1><a href="<?php bloginfo("url") ?>"><?php bloginfo('name')?></a></h1>
             <h2><?php bloginfo('description')?></h2>
-            <?php
+            <div class="entete__nav">
+                <?php
                 wp_nav_menu(array(
                                         "menu" => "principal",
                                         "container" => "nav"
                                     )); ?>
-            <form action="#" class="recherche">
+            </div>
+            <div class="entete__search_form">
+                <?php
+                get_search_form();
+            ?>
+            </div>
+            <!-- <form action="#" class="recherche">
                 <input type="search" name="" id="">
                 <img src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="32" height="32">
-            </form>
+            </form> -->
         </section>
     </header>

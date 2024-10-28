4a23b0ca531e3c06a4d58b85c7cfb3b55fd0ba95
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2024-10-28 01:15:15 -0400

    ajout de front-page, search, modification de l'index, et du style de ma barre de recherche. ainsi que l'affichage des articles dans les pages. si seulement un element, prendre plus de place, sinon, prendre 3 rangé.

--- front-page.php
+++ front-page.php
@@ -3,10 +3,10 @@
 ?>
 <main class="principal">
     <section class="global">
-        <h2>index</h2>
+        <h2>Front Page</h2>
         <div class="principal__conteneur">
-            <?php// if (have_posts()) : ?>
-            <?php// while (have_posts()) : the_post() ?>
+            <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post() ?>
             <?php
                 $chaine = get_the_title();
                 $sigle = substr($chaine,0,7);
@@ -14,18 +14,18 @@
             ?>
             <article class="principal__article">
                 <h5><a href="<?= the_permalink() ?>"><?= $sigle . " " . $titre ?></a></h5>
-                <p><?= get_the_content() ?></p>
                 <?php
                     $pos_ouvrante = stripos($chaine, "(");
                     if ($pos_ouvrante !== false) {
                         $heureDemandé = substr($chaine, $pos_ouvrante + 1, -1);
                     }
                 ?>
-                <small>(<?= $heureDemandé ?>)</small>
+                <small><strong>(<?= $heureDemandé ?>)</strong></small>
+                <p><?php  echo wp_trim_words(get_the_excerpt(), 30, null); ?></p>
             </article>
-            <?php// endwhile; ?>
+            <?php endwhile; ?>
         </div>
-        <?php// endif ?>
+        <?php endif ?>
     </section>
 </main>
 <?php
--- search.php
+++ search.php
@@ -3,29 +3,29 @@
 ?>
 <main class="principal">
     <section class="global">
-        <h2>index</h2>
+        <h2>Résultat de la recherche</h2>
         <div class="principal__conteneur">
-            <?php// if (have_posts()) : ?>
-            <?php// while (have_posts()) : the_post() ?>
+            <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post() ?>
             <?php
                 $chaine = get_the_title();
                 $sigle = substr($chaine,0,7);
                 $titre = substr($chaine, 8,stripos($chaine,"(")-8);
             ?>
-            <article class="principal__article">
+            <article class="principal__recherche">
                 <h5><a href="<?= the_permalink() ?>"><?= $sigle . " " . $titre ?></a></h5>
-                <p><?= get_the_content() ?></p>
                 <?php
                     $pos_ouvrante = stripos($chaine, "(");
                     if ($pos_ouvrante !== false) {
                         $heureDemandé = substr($chaine, $pos_ouvrante + 1, -1);
                     }
                 ?>
-                <small>(<?= $heureDemandé ?>)</small>
+                <small><strong>(<?= $heureDemandé ?>)</strong></small>
+                <p><?php  echo wp_trim_words(get_the_excerpt(), 80, null); ?></p>
             </article>
-            <?php// endwhile; ?>
+            <?php endwhile; ?>
         </div>
-        <?php// endif ?>
+        <?php endif ?>
     </section>
 </main>
 <?php

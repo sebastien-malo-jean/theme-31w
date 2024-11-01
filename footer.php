dfecc8328b0b5f9eb3a7dcde7fba40c39ea4602a
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2024-11-01 01:43:53 -0400

    changer le message dans le footer;

--- footer.php
+++ footer.php
@@ -23,10 +23,14 @@
                         src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png" alt="github"></a>
             </div>
             <div class="colonne2">
-                <p>Cet exercice a pour objectif d’enrichir et de perfectionner le thème
-                    WordPress en intégrant divers modèles, en ajoutant des fonctionnalités
-                    de style et de recherche, ainsi qu’en structurant le pied de
-                    page.</p>
+                <p>Cet exercice consiste à améliorer un thème WordPress en apportant des modifications à plusieurs
+                    modèles et en intégrant des fonctionnalités avancées. Les objectifs incluent l’optimisation de la
+                    page d'accueil front-page.php pour n'afficher que les articles de la catégorie "Nouvelle" en
+                    modifiant la requête principale avec le hook pre_get_posts, ainsi que la création des modèles
+                    category-cours.php et 404.php. Le modèle category-cours.php affichera les cours du département TIM
+                    avec les noms des professeurs, tandis que le modèle 404.php présentera un message d'erreur et une
+                    liste simplifiée des cours avec des sigles cliquables pour accéder à leurs descriptions complètes.
+                    Le pied de page, footer.php, sera enrichi d'une liste de liens vers des cours Web spécifiques.</p>
 
                 <a href="https://www.cmaisonneuve.qc.ca" class="pied__img-frame">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY52p9xHQBK5npgsurkiQvfStx-8Tb3O6R-g&s"

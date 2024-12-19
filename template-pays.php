c9059ab1fd76c21347d0a00e7a848bc23af22243
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2024-12-19 10:57:02 -0500

    mise en place de la page template-pays.

---
+++ template-pays.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Template Pays
+*/
+?>
+<?php
+    get_header();
+?>
+<main class="principal">
+    <section class="global">
+        <h2>Destination par pays</h2>
+        <p>Découvrez les plus belles destinations à travers le monde, classées par pays. Nous avons soigneusement
+            sélectionné les pays les plus populaires pour vous offrir une expérience unique et inoubliable.</p>
+        <div class="principal__conteneur">
+            <?php
+            ?>
+            <article class="principal__article">
+                <p>texte</p>
+            </article>
+        </div>
+    </section>
+</main>
+<?php
+    get_footer();
+?>
